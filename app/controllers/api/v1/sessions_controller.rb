e47046dbff4429276e759a873d3cf1214b84bf92
getssh <gtamrat33@gmail.com>
2023-08-16 00:50:07 +0300

    Update routes to include doc and reserv

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+    if user
+      render json: { message: 'Login successful' }, status: :ok
+    else
+      render json: { message: 'User not found' }, status: :not_found
+    end
+  end
+end
