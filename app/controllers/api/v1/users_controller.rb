e41d50d7fb71fdff0148757c101e8e67eae828c7
getssh <gtamrat33@gmail.com>
2023-08-18 08:46:12 +0300

    Add data to a json returning value backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: { message: 'User created successfully' }, status: :created
+      render json: { message: 'User created successfully', data: user }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
