e2efdabf8ef2b041639b0234c8e18669c591e033
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-08-18 21:34:45 +0300

    Change api json responses

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::DoctorsController < ApplicationController
   def index
     doctors = Doctor.all
     if doctors.empty?
-      render json: { status: 'ERROR', message: 'Doctors not found' }, status: :unprocessable_entity
+      render json: [], status: :ok
     else
-      render json: { status: 'SUCCESS', message: 'Loaded all doctors', data: doctors }, status: :ok
+      render json: doctors, status: :ok
     end
   end
 
   def create
     doctor = Doctor.new(doctor_params)
     if doctor.save
-      render json: { status: 'SUCCESS', message: 'Saved doctor', data: doctor }, status: :ok
+      render json: doctor, status: :ok
     else
       render json: { status: 'ERROR', message: 'Doctor not saved', data: doctor.errors }, status: :unprocessable_entity
     end
@@ -19,7 +19,7 @@ class Api::V1::DoctorsController < ApplicationController
 
   def show
     doctor = Doctor.find(params[:id])
-    render json: { status: 'SUCCESS', message: 'Loaded doctor', data: doctor }, status: :ok
+    render json: doctor, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { status: 'ERROR', message: 'Doctor does not exist' }, status: :unprocessable_entity
   end
@@ -27,7 +27,7 @@ class Api::V1::DoctorsController < ApplicationController
   def destroy
     doctor = Doctor.find(params[:id])
     doctor.destroy
-    render json: { status: 'SUCCESS', message: 'Doctor deleted', data: doctor }, status: :ok
+    render json: { status: 'SUCCESS', message: 'Doctor deleted' }, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { status: 'ERROR', message: 'Doctor does not exist' }, status: :unprocessable_entity
   end
