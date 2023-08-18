373eac3fc9b094ab515f663987efef2e295af1f8
getssh <gtamrat33@gmail.com>
2023-08-18 12:01:00 +0300

    Add seed data as a starter

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Doctor.create(
+  name: 'Dr. John Smith',
+  description: 'Experienced cardiologist specializing in heart health.',
+  specialization: 'Cardiology',
+  cost_per_session: 150.0,
+  image_url: 'https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+)
+
+Doctor.create(
+  name: 'Dr. Emily Brown',
+  description: 'Pediatrician with a passion for children\'s health.',
+  specialization: 'Pediatrics',
+  cost_per_session: 120.0,
+  image_url: 'https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+)
+
+Doctor.create(
+  name: 'Dr. Michael Johnson',
+  description: 'Orthopedic surgeon focused on sports injuries.',
+  specialization: 'Orthopedics',
+  cost_per_session: 180.0,
+  image_url: 'https://images.unsplash.com/photo-1537368910025-700350fe46c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+)
+
+Doctor.create(
+  name: 'Dr. Sarah Williams',
+  description: 'Psychiatrist helping patients achieve mental wellness.',
+  specialization: 'Psychiatry',
+  cost_per_session: 200.0,
+  image_url: 'https://images.unsplash.com/photo-1551884170-09fb70a3a2ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1176&q=80'
+)
